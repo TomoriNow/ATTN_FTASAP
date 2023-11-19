3b84282331e71d288a3d58ea7e382df99800ea86
TomoriNow <seanarcher1974@gmail.com>
2023-11-19 18:43:15 +0700

    created django project

---
+++ merah_biru_daycare/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for merah_biru_daycare project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'merah_biru_daycare.settings')
+
+application = get_wsgi_application()
