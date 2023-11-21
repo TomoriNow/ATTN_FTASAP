7a2399e1ed460e3a3e59e85e6abb6c832fe389a2
TomoriNow <seanarcher1974@gmail.com>
2023-11-21 17:23:28 +0700

    added some html pages

--- main/forms.py
+++ main/forms.py
@@ -10,3 +10,10 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = AccountUser
         fields = ('email', 'password1', 'password2')
+        
+class ChildRegisterForm(UserCreationForm):
+    first_name = forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), max_length=32, help_text='First name')
+    last_name=forms.CharField(forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), max_length=32, help_text='Last name')
+    password1=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2=forms.CharField(forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
+    
