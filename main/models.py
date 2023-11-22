2387e17b0a7a7c70cf6c52110b4a6bc434197827
Sirered <gikurniawan.1995@gmail.com>
2023-11-22 11:31:50 +0700

    Child stuff

--- main/models.py
+++ main/models.py
@@ -3,15 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 import uuid
 
+GENDER_CHOICES = [("Male", "Male"),("Female", "Female")]
 class AccountUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     userID = models.UUIDField(default=uuid.uuid4)
     firstName = models.CharField(max_length= 20)
     lastName = models.CharField(max_length= 20)
-    gender = models.CharField(max_length= 10)
+    gender = models.CharField(max_length= 10, choices = GENDER_CHOICES)
     birthDate = models.DateField()
     address = models.TextField()
     is_staff = models.BooleanField(default=False)
+    is_child = models.BooleanField(default=False)
 
 class Staff(models.Model):
     user = models.OneToOneField(AccountUser, on_delete=models.CASCADE)
@@ -19,3 +21,11 @@ class Staff(models.Model):
     NPWP = models.CharField(max_length=50)
     BankAccount = models.CharField(max_length=50)
     BankName = models.CharField(max_length=50)
+
+class Child(models.Model):
+    user = models.OneToOneField(AccountUser, on_delete=models.CASCADE)
+    dadName = models.CharField(max_length=50)
+    momName = models.CharField(max_length=50)
+    dadJob = models.CharField(max_length=20)
+    momJob = models.CharField(max_length=20)
+    
--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, login_user, register_admin, register
+from main.views import show_main, login_user, register_admin, register, register_staff, dashboard, register_child
 
 app_name = 'main'  # Set the app namespace if necessary
 
@@ -8,4 +8,7 @@ urlpatterns = [
     path('login/', login_user, name='login_user'),
     path('register/', register, name='register'),
     path('register/admin/', register_admin, name='register_admin'),
+    path('register/staff/', register_staff, name='register_staff'),
+    path('register/child/', register_child, name='register_child'),
+    path('dashboard', dashboard, name = 'dashboard')
 ]
