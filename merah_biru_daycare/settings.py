9b078e8096abbfc049583c37dccf6563a72004d2
TomoriNow <seanarcher1974@gmail.com>
2023-12-26 15:27:00 +0700

    DEPLOY PLS (AGAIN)

--- merah_biru_daycare/settings.py
+++ merah_biru_daycare/settings.py
@@ -130,3 +130,7 @@ STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#CSRF Stuff
+
+CSRF_TRUSTED_ORIGINS = ['web-production-a624.up.railway.app']
